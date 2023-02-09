4c92497ceb25b55a6c5682800ccabd9dd63ed598
KristenSegeren <krissege449@ed.amdsb.ca>
2023-02-09 14:42:05 -0500

    Add project files.

---
+++ Game.Start/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Game.Start
+{
+    partial class YourMOM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.STARTbutton = new System.Windows.Forms.Button();
+            this.Name1Label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NewBGlabel = new System.Windows.Forms.Label();
+            this.LabelBG = new System.Windows.Forms.Label();
+            this.GOlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // STARTbutton
+            // 
+            this.STARTbutton.BackColor = System.Drawing.Color.Salmon;
+            this.STARTbutton.Font = new System.Drawing.Font("Kristen ITC", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STARTbutton.ForeColor = System.Drawing.Color.Transparent;
+            this.STARTbutton.Location = new System.Drawing.Point(2, 52);
+            this.STARTbutton.Name = "STARTbutton";
+            this.STARTbutton.Size = new System.Drawing.Size(802, 75);
+            this.STARTbutton.TabIndex = 0;
+            this.STARTbutton.Text = "REALLY COOL GAME STUFF";
+            this.STARTbutton.UseVisualStyleBackColor = false;
+            // 
+            // Name1Label
+            // 
+            this.Name1Label.AutoSize = true;
+            this.Name1Label.BackColor = System.Drawing.Color.Salmon;
+            this.Name1Label.Font = new System.Drawing.Font("Kristen ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name1Label.ForeColor = System.Drawing.Color.Snow;
+            this.Name1Label.Location = new System.Drawing.Point(278, 213);
+            this.Name1Label.Name = "Name1Label";
+            this.Name1Label.Size = new System.Drawing.Size(263, 83);
+            this.Name1Label.TabIndex = 1;
+            this.Name1Label.Text = "START";
+            this.Name1Label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(89, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 135);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3";
+            this.label3.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(309, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 135);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(531, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 135);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // NewBGlabel
+            // 
+            this.NewBGlabel.AutoSize = true;
+            this.NewBGlabel.Location = new System.Drawing.Point(232, 194);
+            this.NewBGlabel.Name = "NewBGlabel";
+            this.NewBGlabel.Size = new System.Drawing.Size(0, 16);
+            this.NewBGlabel.TabIndex = 5;
+            // 
+            // LabelBG
+            // 
+            this.LabelBG.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.LabelBG.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelBG.ForeColor = System.Drawing.Color.White;
+            this.LabelBG.Location = new System.Drawing.Point(-29, -48);
+            this.LabelBG.Name = "LabelBG";
+            this.LabelBG.Size = new System.Drawing.Size(863, 523);
+            this.LabelBG.TabIndex = 6;
+            this.LabelBG.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // GOlabel
+            // 
+            this.GOlabel.AutoSize = true;
+            this.GOlabel.BackColor = System.Drawing.Color.DarkCyan;
+            this.GOlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GOlabel.ForeColor = System.Drawing.Color.White;
+            this.GOlabel.Location = new System.Drawing.Point(149, 158);
+            this.GOlabel.Name = "GOlabel";
+            this.GOlabel.Size = new System.Drawing.Size(507, 135);
+            this.GOlabel.TabIndex = 7;
+            this.GOlabel.Text = "GO!!!!!!!!";
+            // 
+            // YourMOM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GOlabel);
+            this.Controls.Add(this.LabelBG);
+            this.Controls.Add(this.NewBGlabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Name1Label);
+            this.Controls.Add(this.STARTbutton);
+            this.Name = "YourMOM";
+            this.Text = "YOUR MOM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button STARTbutton;
+        private System.Windows.Forms.Label Name1Label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NewBGlabel;
+        private System.Windows.Forms.Label LabelBG;
+        private System.Windows.Forms.Label GOlabel;
+    }
+}
+
---
+++ Game.Start/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game.Start
+{
+    public partial class YourMOM : Form
+    {
+        public YourMOM()
+        {
+            InitializeComponent();
+            label3.Visible= false;
+            label2.Visible= false;
+            label1.Visible= false;
+            LabelBG.Visible= false;
+            GOlabel.Visible= false; 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            STARTbutton.Visible = false; //startButton
+            Name1Label.Visible = false;  //nameLabel
+
+            SoundPlayer _321sound = new SoundPlayer(Properties.Resources._321Sound);
+            _321sound.Play();
+
+            label3.Visible = true;
+            Refresh();
+            Thread.Sleep(1000);
+
+            label2.Visible = true;
+            Refresh();
+            Thread.Sleep(1000);
+
+            label1.Visible = true;
+            Refresh();
+            Thread.Sleep(1000);
+
+            _321sound.Stop();
+
+            label3.Visible = false;
+            label2.Visible = false;
+            label1.Visible = false;
+
+            BackColor = Color.MediumAquamarine;
+            GOlabel.Visible = true;
+
+            SoundPlayer CatScream = new SoundPlayer(Properties.Resources.CatScream);
+            CatScream.Play();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            STARTbutton.Visible = false; 
+            Name1Label.Visible = false;
+            label3.Visible = true;
+            Refresh();
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }       
+    
+}
